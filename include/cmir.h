5551afbbd102f0e7b6e478e3334c6a241c195884
kcmw3e <kcmw3e@gmail.com>
2022-06-04 15:00:00 -0400

    add cmir main soruce files

---
+++ include/cmir.h
@@ -0,0 +1,18 @@
+// license
+// ---------------------------------------------------------------------------------------------- //
+// Copyright (c) 2022, Casey Walker
+// All rights reserved.
+// 
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+//
+//
+// cmir.h
+// ---------------------------------------------------------------------------------------------- //
+// A library to handle window stuff for different platforms.
+
+#ifndef CMIR_H
+#define CMIR_H
+
+
+#endif // CMIR_H
